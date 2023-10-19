0d6ef7dc316e5c01b15c97e281e525f41beec32c
egesua <egesu06@gmail.com>
2023-10-19 15:18:46 +0300

    first step

--- src/App.js
+++ src/App.js
@@ -2,21 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const username = "Ege Su";
+ 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Merhaba {username}</h1>
+        <hr></hr>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Merhaba.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
